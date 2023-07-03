ff896d94219c3ccbd13d4268c1b004b7133ebfc6
Amadi <paulamadi39@gmail.com>
2023-07-03 12:21:13 -0700

    es6 data manipulation

---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,9 @@
+/**
+ * Creates a set from an array.
+ * @param {Array} array - The source array.
+ * @author Amadi Paul <https://github.com/Palanii1>
+ * @returns {Set}
+ */
+export default function setFromArray(array) {
+  return new Set(array);
+}
